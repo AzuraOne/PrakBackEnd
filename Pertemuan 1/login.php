cec019789d9ca2d4a866a7ade8590840f5e6ca96
AzuraOne <narutokah1@gmail.com>
2021-08-27 10:00:56 +0700

    Membuat login, logout dan register

--- Pertemuan 1/login.php
+++ Pertemuan 1/login.php
@@ -1,6 +1,11 @@
 <?php 
+error_reporting(0);
 session_start();
 
+
+$name = $_SESSION['uname'];
+$passw = $_SESSION['passwd'];
+$stats = $_SESSION['status'];
 
 ?>
 <!DOCTYPE html>
@@ -12,7 +17,33 @@ session_start();
     <title>Login Page</title>
 </head>
 <body>
+    
+    <?php if ($stats == 1) {
+        ?><h1><a href="logout.php">Logout?</a></h1> <?php
+     ?>
     <form action="process.php" method="post">
+
+        <ul>
+            <li>
+                <label for="uname">Masukkan Username</label>
+                <input type="text" name="uname" id="uname" placeholder="Username" value="<?php echo $name?>" required>
+
+            </li>
+            
+            <li>
+                <label for="passwd">Masukkan Password</label>
+                <input type="passwd" name="passwd" id="passwd" placeholder="password" value="<?php echo $passw?>" required>
+                
+            </li>
+            <li>
+                <button type="submit">Login</button>
+            </li>
+        </ul>
+
+    </form>
+    <?php }else { ?>
+        <form action="process.php" method="post">
+
         <ul>
             <li>
                 <label for="uname">Masukkan Username</label>
@@ -31,6 +62,8 @@ session_start();
         </ul>
 
     </form>
+        <?php 
+            }?>
     
 </body>
 </html>
--- Pertemuan 1/process.php
+++ Pertemuan 1/process.php
@@ -9,6 +9,11 @@
     if ($truename = $uname) {
         if ($truepasswd == $passwd) {
             echo "Username dan password Sama";
+            $_SESSION['uname'] = $uname;
+            $_SESSION['passwd'] = $passwd;
+            $_SESSION['status'] = 1;
+            header( "Location: login.php" );
+
         }else {
             echo "Password tidak Sama";
         }
